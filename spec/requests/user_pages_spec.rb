0aa2077f56ab8e0a6ca9da90fec23b37cea43e2e
Brandt Ryan <emailme@brandtryan.com>
2014-04-22 06:26:24 -0400

    add cucumber

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,7 +10,7 @@ describe "UserPages" do
 
 		it { should have_content(user.name) }
 		it { should have_title(user.name) }
-end
+	end
 
 	describe "signup page" do
 		before { visit signup_path }
@@ -30,6 +30,13 @@ end
 				expect { click_button submit }.not_to change(User, :count)
 			end
 		end
+	
+			describe "after submission" do
+				before { click_button submit }
+
+				it { should have_title('Sign up') }
+				it { should have_content('error') }
+			end
 
 		describe "with valid information" do
 			before do
@@ -41,6 +48,14 @@ end
 
 			it "should create a user" do
 				expect { click_button submit }.to change(User, :count)
+			end
+
+			describe "after saving the user" do
+				before { click_button submit }
+				let(:user) { User.find_by(email: 'user@example.com') }
+
+				it { should have_title(user.name) }
+				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
 			end
 		end
 	end
